8db912d3fb05dc03e3fd9e8a40879003be7dd6bc
rupeshkashyap <rupesh.kashyap@agiledgesolutions.com>
2020-05-09 00:25:51 +0530

    adding Testlife Cycle @BeforeAll, @BeforeEach, @AfterEach, @AfterAll

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Basic Junit5 project
 2. Jupitor API
 3. Sure Fire 
 
+# Importent point 
+1. for evey  test case junit-5 crete new instacce so don't create class member valiabe and avoid the order of taes case execution
+2. Test Life cycle : - lifecucle hooks 
+	a. @BeforeAll :- Executing before any thing will execute, before even class instacce created , that means we can call only static method
+	b. @AfterAll :- method will call after the last test case
+	c. @BeforeEach :- method will ececute before ececute every test cases
+	d. @AfterEach :- method will ececute after ececute every test cases
+
--- src/test/java/com/rupesh/MathUtilsTest.java
+++ src/test/java/com/rupesh/MathUtilsTest.java
@@ -2,16 +2,37 @@ package com.rupesh;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MathUtilsTest {
+	
+	MathUtils mathUtils ;
+	
+	@BeforeAll
+	static void beforeAll() {
+	System.out.println("This needs to run before all");
+	}
+	
+	@BeforeEach
+	void init() {
+	 mathUtils = new MathUtils();
+
+	}
+	
+	@AfterEach
+	void cleaup() {
+		System.out.println("Cleanup...");
+	}
 
 //	telling this this method in for testing
 	@Test
 	void testAdd() {
-		int actual = new MathUtils().add(1, 1);
+		int actual = mathUtils .add(1, 1);
 		int expected = 2;
 		assertEquals(expected, actual,"The add() method should add two number");
 	}
@@ -20,8 +41,9 @@ class MathUtilsTest {
 	void devide() {
 //		for exception handling we  are user assertThrows
 		//  lambda expression not supported in junit 4 for ssertThrow 
-		MathUtils mathUtils = new MathUtils();
-		assertThrows(NullPointerException.class, () -> mathUtils.devide(1, 0),"Devide by zero should throw");
+//		MathUtils mathUtils = new MathUtils();
+//		assertThrows(NullPointerException.class, () -> mathUtils.devide(1, 0),"Devide by zero should throw");
+		assertThrows(ArithmeticException.class, () -> mathUtils.devide(1, 0),"Devide by zero should throw"); // correct 
 		
 	}
 	
