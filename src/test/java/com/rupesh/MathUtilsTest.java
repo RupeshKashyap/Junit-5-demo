fddbf71cf84b42f5aaf7eeb3b40f8d3e73153887
rupeshkashyap <rupesh.kashyap@agiledgesolutions.com>
2020-05-08 23:49:06 +0530

    adding exception hadling using assertThrows

--- src/test/java/com/rupesh/MathUtilsTest.java
+++ src/test/java/com/rupesh/MathUtilsTest.java
@@ -3,7 +3,9 @@ package com.rupesh;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class MathUtilsTest {
 
 //	telling this this method in for testing
@@ -15,8 +17,18 @@ class MathUtilsTest {
 	}
 	
 	@Test
+	void devide() {
+//		for exception handling we  are user assertThrows
+		//  lambda expression not supported in junit 4 for ssertThrow 
+		MathUtils mathUtils = new MathUtils();
+		assertThrows(NullPointerException.class, () -> mathUtils.devide(1, 0),"Devide by zero should throw");
+		
+	}
+	
+	@Test
 	void testComputeCircleArea() {
 		assertEquals(78.53981633974483, new MathUtils().computeCircleArea(5));
 	}
+	
 
 }
